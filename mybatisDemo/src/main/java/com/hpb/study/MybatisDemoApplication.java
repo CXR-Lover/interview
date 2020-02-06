2259a20f4b9fe7f14678462c50977b0c86d8d956
hongpengbo <1124243928Wqq.com>
2020-02-06 11:38:25 +0800

    基础知识

---
+++ mybatisDemo/src/main/java/com/hpb/study/MybatisDemoApplication.java
@@ -0,0 +1,18 @@
+package com.hpb.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Classname MybatisDemoApplication
+ * @Description TODO
+ * @Date 2020/2/6 11:29
+ * @Created by CXR
+ */
+
+@SpringBootApplication
+public class MybatisDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisDemoApplication.class);
+    }
+}
